19e4e786d49ceb8de61cc7857b012158aec6d7f3
GyaneshwerJha <keshavk72411@gmail.com>
2023-11-16 10:40:11 +0530

    reversed

--- controller/Auth.js
+++ controller/Auth.js
@@ -1,65 +1,31 @@
-// Import necessary modules
-const bcrypt = require('bcrypt');
-const { User } = require('../model/User'); // Import your User model
+const { User } = require('../model/User');
 
-// Controller function for user signup
 exports.createUser = async (req, res) => {
-  const { email, password } = req.body;
-
+  const user = new User(req.body);
   try {
-    // Check if the user already exists
-    const existingUser = await User.findOne({ email });
-
-    if (existingUser) {
-      return res.status(400).json({ message: 'User already exists' });
-    }
-
-    // Hash the password
-    const saltRounds = 10;
-    const hashedPassword = await bcrypt.hash(password, saltRounds);
-
-    // Create a new user
-    const newUser = new User({
-      email,
-      password: hashedPassword,
-      // Include other user properties as needed
-    });
-
-    // Save the user to the database
-    await newUser.save();
-
-    res.status(201).json({ message: 'User created successfully' });
-  } catch (error) {
-    console.error('Error in createUser:', error);
-    res.status(500).json({ message: 'Internal server error' });
+    const doc = await user.save();
+    res.status(201).json({ id: doc.id, role: doc.role });
+  } catch (err) {
+    res.status(400).json(err);
   }
 };
 
-// Controller function for user login
 exports.loginUser = async (req, res) => {
-  const { email, password } = req.body;
-
   try {
-    // Find the user by email
-    const user = await User.findOne({ email });
-
+    const user = await User.findOne(
+      { email: req.body.email },
+    ).exec();
+    // TODO: this is just temporary, we will use strong password auth
+    console.log({ user })
     if (!user) {
-      return res.status(401).json({ message: 'Invalid credentials' });
+      res.status(401).json({ message: 'no such user email' });
+    } else if (user.password === req.body.password) {
+      // TODO: We will make addresses independent of login
+      res.status(200).json({ id: user.id, role: user.role });
+    } else {
+      res.status(401).json({ message: 'invalid credentials' });
     }
-
-    // Compare entered password with the hashed password in the database
-    const isPasswordValid = await bcrypt.compare(password, user.password);
-
-    if (!isPasswordValid) {
-      return res.status(401).json({ message: 'Invalid credentials' });
-    }
-
-    // Password is valid, proceed with creating a session or generating a JWT
-    // ...
-
-    res.status(200).json({ message: 'Login successful' });
-  } catch (error) {
-    console.error('Error in loginUser:', error);
-    res.status(500).json({ message: 'Internal server error' });
+  } catch (err) {
+    res.status(400).json(err);
   }
 };
--- model/User.js
+++ model/User.js
@@ -1,38 +1,20 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
-const bcrypt = require('bcrypt');
 
 const userSchema = new Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  role: { type: String, required: true, default: 'user' },
-  addresses: { type: [Schema.Types.Mixed] },
+  role: { type: String, required: true, default:'user' },
+  addresses: { type: [Schema.Types.Mixed] }, 
+  // TODO:  We can make a separate Schema for this
   name: { type: String },
   orders: { type: [Schema.Types.Mixed] }
-});
-
-// Add the pre-save middleware for password hashing
-userSchema.pre('save', async function (next) {
-  try {
-    console.log('Middleware triggered');
-    console.log('Plain text password:', this.password);
-
-    const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(this.password, salt);
-    this.password = hashedPassword;
-
-    next();
-  } catch (error) {
-    console.error('Error in middleware:', error);
-    next(error);
-  }
 });
 
 const virtual = userSchema.virtual('id');
 virtual.get(function () {
   return this._id;
 });
-
 userSchema.set('toJSON', {
   virtuals: true,
   versionKey: false,
