223f72033b68d0d48de5679d349a4a0ea35d22a5
GyaneshwerJha <keshavk72411@gmail.com>
2023-11-16 09:54:59 +0530

    password-hashed-completed

--- model/User.js
+++ model/User.js
@@ -1,30 +1,38 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
+const bcrypt = require('bcrypt');
 
 const userSchema = new Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  role: { type: String, required: true, default:'user' },
-  addresses: { type: [Schema.Types.Mixed] }, 
-  // TODO:  We can make a separate Schema for this
+  role: { type: String, required: true, default: 'user' },
+  addresses: { type: [Schema.Types.Mixed] },
   name: { type: String },
   orders: { type: [Schema.Types.Mixed] }
 });
-// Hash the password before saving to the database
-userSchema.pre('save', async function(next) {
+
+// Add the pre-save middleware for password hashing
+userSchema.pre('save', async function (next) {
   try {
+    console.log('Middleware triggered');
+    console.log('Plain text password:', this.password);
+
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(this.password, salt);
     this.password = hashedPassword;
+
     next();
   } catch (error) {
+    console.error('Error in middleware:', error);
     next(error);
   }
 });
+
 const virtual = userSchema.virtual('id');
 virtual.get(function () {
   return this._id;
 });
+
 userSchema.set('toJSON', {
   virtuals: true,
   versionKey: false,
