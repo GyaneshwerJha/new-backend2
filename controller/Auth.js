c3d7dd8f6ddf0f5e2ac2dbba7235b4947d052fcf
GyaneshwerJha <keshavk72411@gmail.com>
2023-11-16 10:02:35 +0530

    password-check-completed

--- controller/Auth.js
+++ controller/Auth.js
@@ -1,31 +1,65 @@
-const { User } = require('../model/User');
+// Import necessary modules
+const bcrypt = require('bcrypt');
+const { User } = require('../models/User'); // Import your User model
 
+// Controller function for user signup
 exports.createUser = async (req, res) => {
-  const user = new User(req.body);
+  const { email, password } = req.body;
+
   try {
-    const doc = await user.save();
-    res.status(201).json({id:doc.id,role:doc.role});
-  } catch (err) {
-    res.status(400).json(err);
+    // Check if the user already exists
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      return res.status(400).json({ message: 'User already exists' });
+    }
+
+    // Hash the password
+    const saltRounds = 10;
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
+
+    // Create a new user
+    const newUser = new User({
+      email,
+      password: hashedPassword,
+      // Include other user properties as needed
+    });
+
+    // Save the user to the database
+    await newUser.save();
+
+    res.status(201).json({ message: 'User created successfully' });
+  } catch (error) {
+    console.error('Error in createUser:', error);
+    res.status(500).json({ message: 'Internal server error' });
   }
 };
 
+// Controller function for user login
 exports.loginUser = async (req, res) => {
+  const { email, password } = req.body;
+
   try {
-    const user = await User.findOne(
-      { email: req.body.email },
-    ).exec();
-    // TODO: this is just temporary, we will use strong password auth
-    console.log({user})
+    // Find the user by email
+    const user = await User.findOne({ email });
+
     if (!user) {
-      res.status(401).json({ message: 'no such user email' });
-    } else if (user.password === req.body.password) {
-        // TODO: We will make addresses independent of login
-      res.status(200).json({id:user.id, role:user.role});
-    } else {
-      res.status(401).json({ message: 'invalid credentials' });
+      return res.status(401).json({ message: 'Invalid credentials' });
     }
-  } catch (err) {
-    res.status(400).json(err);
+
+    // Compare entered password with the hashed password in the database
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordValid) {
+      return res.status(401).json({ message: 'Invalid credentials' });
+    }
+
+    // Password is valid, proceed with creating a session or generating a JWT
+    // ...
+
+    res.status(200).json({ message: 'Login successful' });
+  } catch (error) {
+    console.error('Error in loginUser:', error);
+    res.status(500).json({ message: 'Internal server error' });
   }
 };
